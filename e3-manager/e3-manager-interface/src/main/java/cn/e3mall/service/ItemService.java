b42322e04f4cf4316f872ca608fb43157ed50c8f
liangwei <liang645669081@126.com>
2017-12-29 20:31:05 +0800

    此次将工程的服务层和表现层进行了分开，将web模块从manager中抽离出作为了单独的工程，将service层打为war包作为服务层，web作为表现层，中间使用了dobbo进行通信，使用zookpper作为注册中心

--- e3-manager/e3-manager-interface/src/main/java/cn/e3mall/service/ItemService.java
+++ e3-manager/e3-manager-interface/src/main/java/cn/e3mall/service/ItemService.java
@@ -1,7 +1,9 @@
 package cn.e3mall.service;
 
+import cn.e3mall.common.DataGridResult;
 import cn.e3mall.pojo.TbItem;
 
 public interface ItemService {
 	public TbItem getItemById(long id);
+	public DataGridResult getItemList(int page,int rows);
 }
--- e3-manager/e3-manager-service/src/main/java/cn/e3mall/service/impl/ItemServiceImpl.java
+++ e3-manager/e3-manager-service/src/main/java/cn/e3mall/service/impl/ItemServiceImpl.java
@@ -1,10 +1,17 @@
 package cn.e3mall.service.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+
+import cn.e3mall.common.DataGridResult;
 import cn.e3mall.mapper.TbItemMapper;
 import cn.e3mall.pojo.TbItem;
+import cn.e3mall.pojo.TbItemExample;
 import cn.e3mall.service.ItemService;
 
 /**
@@ -21,6 +28,26 @@ public class ItemServiceImpl implements ItemService {
 	@Override
 	public TbItem getItemById(long id) {
 		return itemMapper.selectByPrimaryKey(id);
+	}
+
+	@Override
+	public DataGridResult getItemList(int page, int rows) {
+		//设置分页信息
+		PageHelper.startPage(page, rows);
+		
+		//执行查询
+		TbItemExample example=new TbItemExample();
+		List<TbItem> list = itemMapper.selectByExample(example);
+		
+		//取分页信息
+		PageInfo<TbItem> pageInfo=new PageInfo<TbItem>(list);
+		
+		long total = pageInfo.getTotal();
+		
+		DataGridResult result=new DataGridResult();
+		result.setTotal(total);
+		result.setRows(list);
+		return result;
 	}
 
 }
